8e10638439d4d67f3fc4918c6bd62329a832bb3e
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-02-27 12:33:22 -0500

    finish up #1 hopefully, this should absorb almost all the complex ethernet synth

---
+++ iprepo/udp_complete_64/xgui/udp_complete_64_v1_0.tcl
@@ -0,0 +1,127 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  ipgui::add_param $IPINST -name "ARP_CACHE_ADDR_WIDTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "ARP_REQUEST_RETRY_COUNT" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "UDP_CHECKSUM_GEN_ENABLE" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "UDP_CHECKSUM_HEADER_FIFO_DEPTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "ARP_CLOCK_PER_SEC" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "ARP_REQUEST_TIMEOUT_SEC" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "ARP_REQUEST_RETRY_INTERVAL_SEC" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH" -parent ${Page_0}
+
+
+}
+
+proc update_PARAM_VALUE.ARP_CACHE_ADDR_WIDTH { PARAM_VALUE.ARP_CACHE_ADDR_WIDTH } {
+	# Procedure called to update ARP_CACHE_ADDR_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.ARP_CACHE_ADDR_WIDTH { PARAM_VALUE.ARP_CACHE_ADDR_WIDTH } {
+	# Procedure called to validate ARP_CACHE_ADDR_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.ARP_CLOCK_PER_SEC { PARAM_VALUE.ARP_CLOCK_PER_SEC } {
+	# Procedure called to update ARP_CLOCK_PER_SEC when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.ARP_CLOCK_PER_SEC { PARAM_VALUE.ARP_CLOCK_PER_SEC } {
+	# Procedure called to validate ARP_CLOCK_PER_SEC
+	return true
+}
+
+proc update_PARAM_VALUE.ARP_REQUEST_RETRY_COUNT { PARAM_VALUE.ARP_REQUEST_RETRY_COUNT } {
+	# Procedure called to update ARP_REQUEST_RETRY_COUNT when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.ARP_REQUEST_RETRY_COUNT { PARAM_VALUE.ARP_REQUEST_RETRY_COUNT } {
+	# Procedure called to validate ARP_REQUEST_RETRY_COUNT
+	return true
+}
+
+proc update_PARAM_VALUE.ARP_REQUEST_RETRY_INTERVAL_SEC { PARAM_VALUE.ARP_REQUEST_RETRY_INTERVAL_SEC } {
+	# Procedure called to update ARP_REQUEST_RETRY_INTERVAL_SEC when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.ARP_REQUEST_RETRY_INTERVAL_SEC { PARAM_VALUE.ARP_REQUEST_RETRY_INTERVAL_SEC } {
+	# Procedure called to validate ARP_REQUEST_RETRY_INTERVAL_SEC
+	return true
+}
+
+proc update_PARAM_VALUE.ARP_REQUEST_TIMEOUT_SEC { PARAM_VALUE.ARP_REQUEST_TIMEOUT_SEC } {
+	# Procedure called to update ARP_REQUEST_TIMEOUT_SEC when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.ARP_REQUEST_TIMEOUT_SEC { PARAM_VALUE.ARP_REQUEST_TIMEOUT_SEC } {
+	# Procedure called to validate ARP_REQUEST_TIMEOUT_SEC
+	return true
+}
+
+proc update_PARAM_VALUE.UDP_CHECKSUM_GEN_ENABLE { PARAM_VALUE.UDP_CHECKSUM_GEN_ENABLE } {
+	# Procedure called to update UDP_CHECKSUM_GEN_ENABLE when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.UDP_CHECKSUM_GEN_ENABLE { PARAM_VALUE.UDP_CHECKSUM_GEN_ENABLE } {
+	# Procedure called to validate UDP_CHECKSUM_GEN_ENABLE
+	return true
+}
+
+proc update_PARAM_VALUE.UDP_CHECKSUM_HEADER_FIFO_DEPTH { PARAM_VALUE.UDP_CHECKSUM_HEADER_FIFO_DEPTH } {
+	# Procedure called to update UDP_CHECKSUM_HEADER_FIFO_DEPTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.UDP_CHECKSUM_HEADER_FIFO_DEPTH { PARAM_VALUE.UDP_CHECKSUM_HEADER_FIFO_DEPTH } {
+	# Procedure called to validate UDP_CHECKSUM_HEADER_FIFO_DEPTH
+	return true
+}
+
+proc update_PARAM_VALUE.UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH { PARAM_VALUE.UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH } {
+	# Procedure called to update UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH { PARAM_VALUE.UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH } {
+	# Procedure called to validate UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.ARP_CACHE_ADDR_WIDTH { MODELPARAM_VALUE.ARP_CACHE_ADDR_WIDTH PARAM_VALUE.ARP_CACHE_ADDR_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.ARP_CACHE_ADDR_WIDTH}] ${MODELPARAM_VALUE.ARP_CACHE_ADDR_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.ARP_REQUEST_RETRY_COUNT { MODELPARAM_VALUE.ARP_REQUEST_RETRY_COUNT PARAM_VALUE.ARP_REQUEST_RETRY_COUNT } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.ARP_REQUEST_RETRY_COUNT}] ${MODELPARAM_VALUE.ARP_REQUEST_RETRY_COUNT}
+}
+
+proc update_MODELPARAM_VALUE.ARP_REQUEST_RETRY_INTERVAL { MODELPARAM_VALUE.ARP_REQUEST_RETRY_INTERVAL } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	# WARNING: There is no corresponding user parameter named "ARP_REQUEST_RETRY_INTERVAL". Setting updated value from the model parameter.
+set_property value 250000000 ${MODELPARAM_VALUE.ARP_REQUEST_RETRY_INTERVAL}
+}
+
+proc update_MODELPARAM_VALUE.ARP_REQUEST_TIMEOUT { MODELPARAM_VALUE.ARP_REQUEST_TIMEOUT } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	# WARNING: There is no corresponding user parameter named "ARP_REQUEST_TIMEOUT". Setting updated value from the model parameter.
+set_property value 1 ${MODELPARAM_VALUE.ARP_REQUEST_TIMEOUT}
+}
+
+proc update_MODELPARAM_VALUE.UDP_CHECKSUM_GEN_ENABLE { MODELPARAM_VALUE.UDP_CHECKSUM_GEN_ENABLE PARAM_VALUE.UDP_CHECKSUM_GEN_ENABLE } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.UDP_CHECKSUM_GEN_ENABLE}] ${MODELPARAM_VALUE.UDP_CHECKSUM_GEN_ENABLE}
+}
+
+proc update_MODELPARAM_VALUE.UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH { MODELPARAM_VALUE.UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH PARAM_VALUE.UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH}] ${MODELPARAM_VALUE.UDP_CHECKSUM_PAYLOAD_FIFO_DEPTH}
+}
+
+proc update_MODELPARAM_VALUE.UDP_CHECKSUM_HEADER_FIFO_DEPTH { MODELPARAM_VALUE.UDP_CHECKSUM_HEADER_FIFO_DEPTH PARAM_VALUE.UDP_CHECKSUM_HEADER_FIFO_DEPTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.UDP_CHECKSUM_HEADER_FIFO_DEPTH}] ${MODELPARAM_VALUE.UDP_CHECKSUM_HEADER_FIFO_DEPTH}
+}
+
