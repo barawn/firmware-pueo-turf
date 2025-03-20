a6c0aec1ca7802a81c6251b476f7aae09406a1a6
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-03-20 18:33:36 -0400

    debuggify the I2C merger

--- hdl/i2c_merger.sv
+++ hdl/i2c_merger.sv
@@ -17,19 +17,27 @@ module i2c_merger(
         inout sda1_out
     );
     
+    parameter DEBUG = "FALSE";
+    
     wire enable_A;
     wire enable_B;    
     
-    i2c_merge_vio u_vio(.clk(clk),
-                         .probe_in0( { scl1_out, scl0_out, scl_in } ),
-                         .probe_in1( { sda1_out, sda0_out, sda_in_o } ),
-                         .probe_out0(enable_A),
-                         .probe_out1(enable_B));
-
-    i2c_merge_ila u_ila(.clk(clk),
-                        .probe0(scl_in),
-                        .probe1(sda_in_t),                        
-                        .probe2(sda_in_o));
+    generate
+        if (DEBUG == "TRUE") begin : DBG
+            i2c_merge_vio u_vio(.clk(clk),
+                                 .probe_in0( { scl1_out, scl0_out, scl_in } ),
+                                 .probe_in1( { sda1_out, sda0_out, sda_in_o } ),
+                                 .probe_out0(enable_A),
+                                 .probe_out1(enable_B));
+            i2c_merge_ila u_ila(.clk(clk),
+                                .probe0(scl_in),
+                                .probe1(sda_in_t),                        
+                                .probe2(sda_in_o));                                         
+        end else begin
+            assign enable_A = 1'b1;
+            assign enable_B = 1'b1;
+        end
+    endgenerate
     
     assign scl0_out = scl_in || !enable_A;
     assign scl1_out = scl_in || !enable_B;
