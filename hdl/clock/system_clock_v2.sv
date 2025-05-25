ce61383b23dc0a540c72325c9a55160ecf5d0257
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-05-25 13:47:51 -0400

    add the x2 clock, start plumbing SURF triggers

--- hdl/clock/system_clock_v2.sv
+++ hdl/clock/system_clock_v2.sv
@@ -3,10 +3,34 @@ module system_clock_v2( input SYS_CLK_P,
                         input SYS_CLK_N,
                         input reset,
                         output sysclk_ibuf_o,
+                        // 125 MHz
                         output sysclk_o,
+                        // 250 MHz, for the master trigger process.
+                        output sysclk_x2_o,
+                        // flag indicating start of the cycle
                         output sysclk_phase_o,
+                        // clock enable indicator for sysclk_x2 to capture sysclk
+                        output sysclk_x2_ce_o,
                         output sysclk_sync_o,
                         output SYNC );
+    // the phase indicators mean we go
+    // sysclk   sysclkx2    sysclk_phase    sysclk_x2_phase_reg     sysclk_x2_phase_rereg sysclk_x2_ce_0
+    // 0        1           0               0                       0                     DLYFF 1
+    // 0        0           0               0                       0                     1
+    // 1        1           DLYFF 1         0                       0                     DLYFF 0
+    // 1        0           1               0                       0                     0
+    // 0        1           1               DLYFF 1                 0                     DLYFF 1
+    // 0        0           1               1                       0                     1
+    // 1        1           DLYFF 0         1                       DLYFF 1               DLYFF 0
+    // 1        0           0               1                       1                     0
+    // 0        1           0               DLYFF 0                 1                     DLYFF 1
+    // 0        0           0               0                       1                     1
+    // so register sysclk_phase
+    // reregister it
+    // if (sysclk_x2_phase_reg && !sysclk_x2_phase_rereg) sysclk_x2_ce <= 0
+    // otherwise sysclk_x2_ce = ~sysclk_x2_ce.
+    // With that we can condition any captures from sysclk to sysclkx2
+    // freely.
 
     parameter INVERT_MMCM = "TRUE";
     parameter GPIO = "TRUE";
@@ -21,7 +45,9 @@ module system_clock_v2( input SYS_CLK_P,
     wire sys_clk_out_p;
     wire sys_clk_out_n;
     wire sys_clk_out = (INVERT_MMCM == "TRUE") ? sys_clk_out_n : sys_clk_out_p;
-
+    // Inversion doesn't matter for the x2 clock, it's the same regardless.
+    wire sys_clk_x2_out;
+    
     wire locked;
 
     // SYSCLK MMCM. No LOC constraint.
@@ -38,11 +64,16 @@ module system_clock_v2( input SYS_CLK_P,
                  .CLKOUT0_PHASE(0.000),
                  .CLKOUT0_DUTY_CYCLE(0.5000),
                  .CLKOUT0_USE_FINE_PS("FALSE"),
+                 .CLKOUT1_DIVIDE(6),
+                 .CLKOUT1_PHASE(0.000),
+                 .CLKOUT1_DUTY_CYCLE(0.5000),
+                 .CLKOUT1_USE_FINE_PS("FALSE"),
                  .CLKIN1_PERIOD(8.000))
             u_mmcm67( .CLKFBOUT( clkfb_out ),
                       .CLKFBIN( clkfb_out_buf ),
                       .CLKOUT0( sys_clk_out_p ),
                       .CLKOUT0B(sys_clk_out_n ),
+                      .CLKOUT1( sys_clk_x2_out),
                       .CLKIN1(  sysclk_ibuf_o ),
                       .CLKIN2(  1'b0 ),
                       .CLKINSEL(1'b1 ),
@@ -68,6 +99,8 @@ module system_clock_v2( input SYS_CLK_P,
     BUFG u_clkfb0_buf(.I(clkfb_out),.O(clkfb_out_buf));
     // Buffer sysclk
     BUFG u_sysclk_buf(.I(sys_clk_out),.O(sysclk_o));
+    // Buffer x2
+    BUFG u_sysclk_x2_buf(.I(sys_clk_x2_out),.O(sysclk_x2_o));
 
     // sysclk = 125 MHz
     // sync = 7.8125 MHz
@@ -80,6 +113,14 @@ module system_clock_v2( input SYS_CLK_P,
     
     // This is the global SYSCLK phase. Its exact value's basically irrelevant.
     reg [3:0] sysclk_phase = {4{1'b0}};
+    
+    reg [1:0] sysclk_phase_rereg_x2 = 2'b00;
+    // clock enable for sysclk_x2 to know it can capture data from sysclk
+    // with max delay.
+    // IOW if sysclk_x2_ce is high the NEXT RISING EDGE
+    // of sysclk_x2 is also a rising edge of sysclk.
+    reg sysclk_x2_ce = 0;
+    
     // ditto for sysclk sync, it doesn't matter that it's random.
     // sysclk = 125 MHz
     // sync = 7.8125 MHz
@@ -94,8 +135,13 @@ module system_clock_v2( input SYS_CLK_P,
         if (sysclk_phase[2:0] == 3'b111) sysclk_sync <= ~sysclk_sync;
         if (sysclk_phase[2:0] == 3'b111) sysclk_sync_obuf <= ~sysclk_sync;
     end
-    
+    always @(posedge sysclk_x2_o) begin
+        sysclk_phase_rereg_x2 <= { sysclk_phase_rereg_x2[0], sysclk_phase };
+        if (sysclk_phase_rereg_x2[0] && !sysclk_phase_rereg_x2[1]) sysclk_x2_ce <= 0;
+        else sysclk_x2_ce <= ~sysclk_x2_ce;
+    end
     assign sysclk_phase_o = sysclk_phase[3];
     assign sysclk_sync_o = sysclk_sync;
+    assign sysclk_x2_ce_o = sysclk_x2_ce;
     assign SYNC = sysclk_sync_obuf;
 endmodule
--- hdl/trig/trig_pueo_wrap.sv
+++ hdl/trig/trig_pueo_wrap.sv
@@ -1,6 +1,8 @@
 `timescale 1ns / 1ps
 `include "interfaces.vh"
-module trig_pueo_wrap(
+module trig_pueo_wrap #(parameter WBCLKTYPE = "NONE",
+                        parameter SYSCLKTYPE = "NONE",
+                        parameter NSURF = 28)(
         input wb_clk_i,
         input wb_rst_i,
         `TARGET_NAMED_PORTS_WB_IF( wb_ , 14, 32 ),
@@ -11,14 +13,28 @@ module trig_pueo_wrap(
         input sysclk_phase_i,
         // this is the 7.8125M sync cycle
         input sysclk_sync_i,
+        // sysclk x2
+        input sysclk_x2_i,
+        // to clean capture from sysclk_i
+        input sysclk_x2_ce_i,
 
         input pps_i,
+        // SOOOOO MANY INPUTS.
+        // SURFs send triggers on a 4-clock cycle, even
+        // though they train on the 8-clock cycle.
+        // They actually send a total of 32 bits per trigger,
+        // but the trigger info is always the first one and
+        // then the following data (which does not have the top bit set)
+        // is 8 bits of metadata.
+        // We then process them in the x2 domain so we get
+        // 8 clocks per cycle, which allows us to multiplex all
+        // of the SURFs into one URAM.
+        input [NSURF*16-1:0] trig_dat_i,
+        input                trig_dat_valid_i,
         
         output [31:0] command67_o,
         output [31:0] command68_o
     );
-    parameter WBCLKTYPE = "NONE";
-    parameter SYSCLKTYPE = "NONE";
     
     // probably add more here or something, or maybe split off
     `DEFINE_AXI4S_MIN_IF( trig_ , 16 );
